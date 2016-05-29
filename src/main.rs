834111aa871c073834e3d5f6239f6bad53ab99f4
Wei Liu <liuw@liuw.name>
2016-05-29 23:46:18 +0100

    Import getopts and basic skeleton
    Signed-off-by: Wei Liu <liuw@liuw.name>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {}", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    let program = args[0].clone();
+
+    opts.optflag("h", "help", "print this help menu");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
     println!("Hello, world!");
 }
